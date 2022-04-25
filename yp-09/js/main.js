d777918ec13413c5e9749e98640909803a5c699d
AlekseieEfremov <101862545+AlekseieEfremov@users.noreply.github.com>
2022-04-25 11:38:33 +0300

    Add files via upload

---
+++ yp-09/js/main.js
@@ -0,0 +1,9 @@
+import modalAppearance from "./modules/modalop.js";
+import modalClose from "./modules/modalcl.js";
+import accordionsOpening from "./modules/opening.js";
+import constructorAccordion from "./modules/constructor.js";
+
+modalAppearance();
+modalClose();
+accordionsOpening();
+constructorAccordion();  
